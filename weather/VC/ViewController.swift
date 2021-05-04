09f1ce7d379355235806a62e9a7f9bc6ffeb1c06
ios_dev <futureiosdeveloper@gmail.com>
2021-05-04 21:40:32 +0300

    fixed errors

--- weather/VC/ViewController.swift
+++ weather/VC/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tempLablel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var cityTF: UITextField!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
     }
 
+    @IBAction func doneButtonPressed() {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
 
 }
 
