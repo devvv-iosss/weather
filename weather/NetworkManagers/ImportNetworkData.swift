ee6a6818d77d80e5f8257903e868cb32fd740a5e
ios_dev <futureiosdeveloper@gmail.com>
2021-05-04 22:15:28 +0300

    ver2

---
+++ weather/NetworkManagers/ImportNetworkData.swift
@@ -0,0 +1,12 @@
+//
+//  ImportNetworkData.swift
+//  weather
+//
+//  Created by Vladimir Syleimanov on 04.05.2021.
+//
+
+import UIKit
+
+let exampleURL = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+
+
